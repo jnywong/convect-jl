4f00d9c11a17b05991d617e5dfc25ea2cc0a99f1
jnywong <jw53119@gmail.com>
2021-08-06 17:48:23 +0100

    fix bug in poisson solver boundary conditions and visualise result via inverse fourier transform

--- src/main.jl
+++ src/main.jl
@@ -1,60 +1,108 @@
 include("myroutines.jl")
 using .myroutines
+using FFTW
 using BenchmarkTools
 using Plots
 
 # Inputs
-nz = 5 # no. of vertical gridpoints
-nn = 3 # no. of Fourier modes 
-a = 1 # L/D aspect ratio
-Ra = 10 # Rayleigh number
+nz = 101 # no. of vertical gridpoints
+nn = 4 # no. of Fourier modes (excluding 0)
+a = 5 # L/D aspect ratio
+Ra = 5000 # Rayleigh number
 Pr = 1 # Prandtl number
-nt = 10 # no. of timesteps
+nt = 10^5 # no. of timesteps
 
 # Domain
-z = range(0,1,length=nz)
+z = LinRange(0,1,nz)
 dz = 1/(nz-1)
 dt = 0.9*dz^2/4 # timestep size (2.19)
+# dt = 3*10^(-6)
 
 # Pre-allocation
-psi = Array{Float64}(undef, (nz,nn))
-global omg = Array{Float64}(undef, (nz,nn)) # FIX GLOBALS: avoid ambiguous scoping by wrapping everything in functions
-global tem = Array{Float64}(undef, (nz,nn))
-dtemdz2 = Array{Float64}(undef, (nz,nn))
-domgdz2 = Array{Float64}(undef, (nz,nn))
-dtemdt = Array{Float64}(undef, (nz,nn,2))
-domgdt = Array{Float64}(undef, (nz,nn,2))
+psi = zeros(nz,nn)
+tem = zeros(nz,nn,2)
+omg = zeros(nz,nn,2)
+dtemdz2 = zeros(nz,nn)
+domgdz2 = zeros(nz,nn)
+dtemdt = zeros(nz,nn,2)
+domgdt = zeros(nz,nn,2)
 
 # Initial conditions
-tem0 = sin.(pi*z) # satisfies (3.6) T(0)=T(1) at time t=0
-global tem = repeat(tem0, outer = [1,nn]) # repeat in x-direction
+for n=1:1:nn
+    tem[:,n,2] = sin.(pi*z) # satisfies (3.6) T(0)=T(1) at time t=0
+end
 
 # Time integration
 global m = 0
+global time = 0
 while m<=nt
     for k=2:1:nz-1
         for n=1:1:nn
             # Second derivatives in tem and omg
-            dtemdz2[k,n] = (tem[k+1,n] - 2*tem[k,n] + tem[k-1,n])/dz^2 # (2.16)
-            domgdz2[k,n] = (omg[k+1,n] - 2*omg[k,n] + omg[k-1,n])/dz^2 # (2.16)
+            dtemdz2[k,n] = (tem[k+1,n,2] - 2*tem[k,n,2] + tem[k-1,n,2])/dz^2 # (2.16)
+            domgdz2[k,n] = (omg[k+1,n,2] - 2*omg[k,n,2] + omg[k-1,n,2])/dz^2 # (2.16)
             # Update time dtemdt and domgdt
-            dtemdt[k,n,2] = (n*pi/a)*psi[k,n] + (dtemdz2[k,n]-(n*pi/a)^2*tem[k,n]) # (3.3)
-            domgdt[k,n,2] = Ra*Pr*(n*pi/a)*tem[k,n] + Pr*(domgdz2[k,n] - (n*pi/a)^2*omg[k,n]) # (3.4)
+            dtemdt[k,n,2] = (n*pi/a)*psi[k,n] + (dtemdz2[k,n]-(n*pi/a)^2*tem[k,n,2]) # (3.3)
+            domgdt[k,n,2] = Ra*Pr*(n*pi/a)*tem[k,n,2] + Pr*(domgdz2[k,n] - (n*pi/a)^2*omg[k,n,2]) # (3.4)
         end
     end
 
     # Update tem and omg using Adams Bashforth time integration
-    global tem = tem + 0.5*dt*(3*dtemdt[:,:,2]-dtemdt[:,:,1]) # (2.18)
-    global omg = omg + 0.5*dt*(3*domgdt[:,:,2]-domgdt[:,:,1]) # (2.18)
+    tem[:,:,2] = tem[:,:,1] + 0.5*dt*(3*dtemdt[:,:,2]-dtemdt[:,:,1]) # (2.18)
+    omg[:,:,2] = omg[:,:,1] + 0.5*dt*(3*domgdt[:,:,2]-domgdt[:,:,1]) # (2.18)
+
     # Update psi using poisson solver
     for n=1:1:nn
-        psi[:,n] = myroutines.poisson(omg[:,n],nz,dz,n,a) # (3.5)
+        psi[:,n] = myroutines.poisson(omg[:,n,2],nz,dz,n,a) # (3.5)
     end
+
+    # Diagnostics
+    println(log(abs(tem[round.(Int,nz/3),1,2]))-log(abs(tem[round.(Int,nz/3),1,1])))
+
+    # Store values for next timestep
     dtemdt[:,:,1] = dtemdt[:,:,2]
     domgdt[:,:,1] = domgdt[:,:,2]
+    tem[:,:,1] = tem[:,:,2]
+    omg[:,:,1] = omg[:,:,2]
+
     global m+=1
+    global time += dt
+
+end
+
+# test = FFTW.idct(tem,2)
+
+# Spectral to spatial in x-direction
+nx = a*nz # no. of horizontal gridpoints
+dx = a/(nx-1)
+x = LinRange(0,a,nx)
+
+# Store cosines and sines
+cosa = zeros(nn,nx)
+sina = zeros(nn,nx)
+for n=1:1:nn
+    cosa[n,:] = cos.(n*pi*x/a)
+    sina[n,:] = sin.(n*pi*x/a)
+end
+
+tem_full = zeros(nz, nx)
+psi_full = zeros(nz, nx)
+# for i=1:1:nx
+#     tem_full[:,i] = fill(1,nz) - z # reference temperature
+# end
+for i = 1:1:nx
+    for k = 1:1:nz
+        for n=1:1:nn
+            tem_full[k,i] += tem[k,n,2]*cosa[n,i]
+            psi_full[k,i] += psi[k,n]*sina[n,i]
+        end
+    end
 end
 
 # Plot
-p = Plots.plot(psi[3,:])
-
+# p = Plots.plot(psi[3,:])
+temLim = maximum(abs.(tem_full))
+p1 = contourf(x, z, tem_full, c=:balance)
+# contour!(x, z, psi_full, c=:grays,levels=50) #,clim=(-temLim,temLim))
+# p2 = contour(x, z, psi_full, c=:grays)
+# plot(p1,p2)
--- src/myroutines.jl
+++ src/myroutines.jl
@@ -3,7 +3,7 @@ module myroutines
     export poisson
     
     function poisson(b,nz,dz,n,a)
-        d = Array{Float64}(undef, nz) # diagonal
+        d = fill(0.0,nz) # diagonal
         du = fill(-1/dz^2, nz-1) # upper diagonal
         dl = fill(-1/dz^2, nz-1) # lower diagonal
         for k=2:1:nz-1
@@ -11,12 +11,12 @@ module myroutines
         end
 
         # Boundary conditions
-        d[1] = 1
-        du[1] = 0
-        d[nz] = 1
-        dl[nz-1] = 0
-        b[1] = 0
-        b[nz] = 0
+        d[1] = 1 # (2.22a)
+        du[1] = 0 #(2.22b)
+        b[1] = 0 # (2.22c)
+        dl[nz-1] = 0 # (2.22d)
+        d[nz] = 1 # (2.22e)
+        b[nz] = 0 # (2.22f)
 
         A = Tridiagonal(dl, d, du)
         x = A\b
