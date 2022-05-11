b84cd86ada642feb70016d213f3bf4da8b8df53b
jnywong <jw53119@gmail.com>
2022-05-11 22:17:40 +0100

    bluebear and log.csv

--- src/convect_nonlinear.jl
+++ src/convect_nonlinear.jl
@@ -3,9 +3,11 @@ include("data_utils.jl")
 using .routines
 using .data_utils
 using Dates
-using BenchmarkTools
-using Plots
-using LaTeXStrings
+using DelimitedFiles
+# using BenchmarkTools
+# using Plots
+# using LaTeXStrings
+t_start = now()
 
 @time begin
     # Inputs
@@ -18,7 +20,7 @@ using LaTeXStrings
     nt = 1e4 # no. of timesteps
     nout = 1e2 # save output every nout timesteps
     initOn = 1 # initialise run, otherwise load existing data
-    saveDir = string("/Users/wongj/Documents/work/convect-out/",today(),"/test") # save directory
+    saveDir = string("/rds/projects/2017/edmondac-rescomp-software/wongj/convect-jl/data/",today()) # save directory
 
     # Vertical domain
     z, dz = routines.zdomain(nz)
@@ -29,3 +31,5 @@ using LaTeXStrings
     # Time integration
     dtemdt, domgdt, tem, omg, psi = routines.nonlinear_solver(z, dz, nz, nn, nt, nout, dt, a, Ra, Pr, psi, tem, omg, initOn, saveDir)
 end
+
+writedlm(string(saveDir,"/log.csv"), string((now()-t_start).value/1000))
